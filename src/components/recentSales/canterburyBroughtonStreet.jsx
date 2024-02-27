6542bb51ad22a16718143569ac9276620325687c
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2024-02-27 21:42:48 +1100

    Add content to sold page

--- src/components/recentSales/canterburyBroughtonStreet.jsx
+++ src/components/recentSales/canterburyBroughtonStreet.jsx
@@ -1,9 +1,14 @@
-// import Carousel from 'react-bootstrap/Carousel';
+import Carousel from 'react-bootstrap/Carousel';
 // import ExampleCarouselImage from 'components/ExampleCarouselImage';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { FaFacebook, FaTwitter } from 'react-icons/fa6';
+import photo1 from '../images/cantebury sale.jpg';
+import photo2 from '../images/canterbury2.jpg';
+import photo3 from '../images/canterbury3.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBed, faBath, faCar } from '@fortawesome/free-solid-svg-icons';
 
 export default function CanterburyBroughtonStreet() {
   return (
@@ -22,17 +27,37 @@ export default function CanterburyBroughtonStreet() {
           </a>
         </div>
       </div>
-      {/* <Carousel>
+      <Carousel>
         <Carousel.Item>
-          <ExampleCarouselImage text="First slide" />
+          <img className="carousel-images" src={photo1} />
         </Carousel.Item>
         <Carousel.Item>
-          <ExampleCarouselImage text="Second slide" />
+          <img className="carousel-images" src={photo2} />
         </Carousel.Item>
         <Carousel.Item>
-          <ExampleCarouselImage text="Third slide" />
+          <img className="carousel-images" src={photo3} />
         </Carousel.Item>
-      </Carousel> */}
+      </Carousel>
+
+      <div className="soldProperties-heading ">
+        <div className="left-container">
+          <FontAwesomeIcon icon={faBed} /> 2
+          <FontAwesomeIcon className="font" icon={faBath} /> 1
+          <h6 className="sold-h6">Sold by Vicki Laing 0411 88 77 60</h6>
+          <p className="sold-p">
+            Located on the first floor of the gracious Art Deco security
+            building "The Oxley", this private rear apartment with its beautiful
+            timber floors, high ceilings and leafy outlook presents a wonderful
+            opportunity to acquire a stunning two bedroom apartment in this
+            sought-after neighbourhood. Ready to move right in and enjoy the
+            vibrancy of Potts Point or lease out as a savvy investment.
+          </p>
+        </div>
+        <div className="top-links right-container">
+          <h5>12/12 Ward Avenue, Potts Point NSW 2011</h5>
+          <h6 className="sold-h6-right">SOLD</h6>
+        </div>
+      </div>
     </div>
   );
 }
